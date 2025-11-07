f936a4efe1481e539b7b6ce9ca14a061eb7ffe9d
wafa-almohsen <75085386+wafa-almohsen@users.noreply.github.com>
2025-11-07 19:03:18 +0300

    Update answer.sql

--- answer.sql
+++ answer.sql
@@ -1,1 +1,15 @@
+SELECT 
+    t.investor_id,
+    s.sector_name,
+    ROUND(
+        t.no_of_shares * 100.0 /
+        SUM(t.no_of_shares) OVER (PARTITION BY t.investor_id),
+        2
+    ) AS share_percentage
+FROM investor_transactions t
+JOIN sectors s 
+    ON t.sector_id = s.sector_id
+ORDER BY 
+    t.investor_id,
+    share_percentage DESC;
 -- ADD YOUR SQL QUERY HERE
